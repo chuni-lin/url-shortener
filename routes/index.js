67cfd7d107968c0c31cfb9d2fdfd86b8f233c29d
chuni-lin <william811566@gmail.com>
2021-01-05 14:52:18 +0800

    feat: modify routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,5 @@ const router = express.Router()
 
 const home = require('./modules/home')
 router.use('/', home)
-// const shorten = require('./modules/shorten')
-// router.use('/shorten', shorten)
-// const code = require('./modules/code')
-// router.use('/', code)
 
 module.exports = router
